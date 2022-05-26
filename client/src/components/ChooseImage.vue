96e7b2fe18791a1c1c796f5badf3f12bf009b7d3
yhait <ghossst2003@gmail.com>
2022-05-26 22:28:40 +0300

    Finish upload information

--- client/src/components/ChooseImage.vue
+++ client/src/components/ChooseImage.vue
@@ -10,9 +10,14 @@
       </div>
     </div>
     <p>{{ filename }}</p>
-    <el-upload ref="uploadRef" class="upload" action="http://127.0.0.1:5000/upload_works" :auto-upload="false"
-      :data="formData" :on-change="handleChange" :show-file-list="false" :limit="3" :on-exceed="handleExceed"
-      :http-request="submitHttp">
+    <el-upload ref="uploadRef" class="upload" 
+      action="http://127.0.0.1:5000/upload_works" 
+      :auto-upload="false"
+      :data="formData" 
+      :on-change="handleChange" 
+      :show-file-list="false" 
+      :limit="1" 
+      :on-exceed="handleExceed">
       <el-button type="primary" class="upload">select file</el-button>
     </el-upload>
     <div class="infor">
@@ -28,56 +33,45 @@
 
 <script lang='ts' setup>
 import { ref } from 'vue';
-import type { UploadFile, UploadFiles, UploadRawFile, UploadRequestOptions } from 'element-plus';
+import type { UploadFile, UploadRawFile } from 'element-plus';
 import type { UploadInstance } from 'element-plus';
 import { genFileId } from 'element-plus';
 import { onBeforeMount } from 'vue';
 import axios from "axios";
 
+interface AuthorData {
+  id:number;
+  name:string;
+}
 const uploadRef = ref<UploadInstance>()
 const filename = ref('')
 const description = ref('')
 const author = ref('')
 const src = ref("#")
 const uploadFileRef = ref<UploadFile>()
-const fileRef = ref<UploadRawFile>()
 const formData = ref<Record<string, any>>()
-const authorOptions = ref([]);
+const authorOptions = ref<AuthorData[]>();
 
 const handleExceed = (files: File[]) => {
-  console.log('file:', files[0])
-  fileRef.value = files[0] as UploadRawFile
   uploadRef.value!.clearFiles()
   const file = files[0] as UploadRawFile
   file.uid = genFileId()
   uploadRef.value!.handleStart(file)
 }
 
-const handleChange = (uploadFile: UploadFile, uploadFiles: UploadFiles) => {
-  fileRef.value = uploadFile.raw!
-  console.log(uploadFile.raw!)
+const handleChange = (uploadFile: UploadFile) => {
   src.value = URL.createObjectURL(uploadFile.raw!)
   filename.value = uploadFile.name
   uploadFileRef.value = uploadFile
-  console.log('handle change:', uploadFile)
-  console.log('files', uploadFiles)
-}
-
-const submitHttp = (options: UploadRequestOptions) => {
-  console.log('submitHttp:', options.file)
-  console.log()
 }
 
 const onSubmit = () => {
-  // uploadRef.value!.submit()
-  // console.log(author.value)
-  // console.log('123', uploadFileRef.value!)
   if (author.value == '') {
-    console.log('请选择创作者。')
+    console.log('Please choose creator!')
     return
   }
   if (description.value!.length == 0) {
-    console.log("请输入描述。")
+    console.log("Please input description of the image!")
     return
   }
   let createTime: number = new Date().getTime()
@@ -85,34 +79,13 @@ const onSubmit = () => {
     'creation_time': createTime,
     'author': author.value!,
     'description': description.value!,
-    'file': fileRef.value!
   }
-  console.log(formData.value)
-  let config = {
-    headers: {
-      'Content-Type': 'multipart/form-data'
-    }
-  }
-  // console.log('uploadRef:', uploadRef.value)
-  // uploadRef.value!.submit()
-  // axios({
-  //   method: 'post',
-  //   url: 'http://127.0.0.1:5000/upload_works',
-  //   data: formData,
-  //   headers: headers
-  // });
-  // console.log(formData)
-  axios.post("http://127.0.0.1:5000/upload_works", formData, config).then((response) => {
-    console.log(response)
-  }).catch((error) => {
-    console.log(error)
-  })
+  uploadRef.value!.submit()
 }
 
 onBeforeMount(() => {
   axios.get("http://127.0.0.1:5000/authors").then((response: { data: any; }) => {
     let da = eval(response.data);
-    console.log(response.data);
     authorOptions.value = da['data']
   })
 })
@@ -122,17 +95,17 @@ onBeforeMount(() => {
 <style scoped>
 .block {
   width: 100%;
-  height: 400px;
+  height: 300px;
 }
 
 .thumb-image {
   width: 100%;
-  height: 400px;
+  height: 100%;
 }
 
 .image-image {
   width: 100%;
-  height: 400px;
+  height: 100%;
   min-width: 100%;
   min-height: 300;
 }
--- client/src/main.ts
+++ client/src/main.ts
@@ -3,5 +3,6 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
+import 'lib-flexible'
 
 createApp(App).use(router).use(ElementPlus).mount('#app')
